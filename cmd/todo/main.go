e4195660ecf21ea9d9dd7576f8565253a2e9a08c
tok1e1 <dan.18.sh@yandex.ru>
2025-02-19 21:58:22 +0400

    feat: add handler

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -3,7 +3,14 @@ package main
 import (
 	"fiberTODO/cmd/database"
 	"fiberTODO/internal/config"
+	"fiberTODO/routes"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"log"
+	"strconv"
 )
 
 func main() {
@@ -29,4 +36,19 @@ func main() {
 	defer db.CloseDB()
 
 	//Реализация обработки на стороне сервера
+	app := fiber.New(fiber.Config{
+		Prefork: true, // используем предварительное форкование для увеличения производительности
+	})
+
+	app.Use(logger.New())   // Логирование запросов
+	app.Use(compress.New()) // Сжатие ответов
+	app.Use(recover.New())  // Восстановление после паники
+	app.Use(limiter.New())  // Лимит запросов для предотвращения DDOS атак
+
+	// Регистрация маршрутов
+	routes.RegisterProductRoutes(app)
+
+	host := cfg.Server.Host
+	port := strconv.Itoa(cfg.Server.Port)
+	log.Fatal(app.Listen(host + ":" + port))
 }
